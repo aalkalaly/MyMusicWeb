b5959129270b3fac0621473f92ec35a243ccf97d
Lenovo <Lenovo@DESKTOP-BMLMVAG>
2024-10-30 22:44:56 +0200

    Add project files.

---
+++ MyMusicWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyMusicWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
