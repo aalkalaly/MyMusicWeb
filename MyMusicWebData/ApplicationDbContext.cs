e5e109c419f680c0aca5a524f3d2dbd529844c51
baharevagabi@gmail.com <baharevagabi@gmail.com>
2024-10-31 12:32:53 +0200

    #add I separated the Data projects

--- MyMusicWebData/ApplicationDbContext.cs
+++ MyMusicWebData/ApplicationDbContext.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace MyMusicWeb.Data
+namespace MyMusicWebData
 {
     public class ApplicationDbContext : IdentityDbContext
     {
